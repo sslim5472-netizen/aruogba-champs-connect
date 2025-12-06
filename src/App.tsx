16abcf3bb1fff5286e6167d94f5cab5afb8fa0e2
sslim5472-netizen <sslim5472@gmail.com>
2025-12-05 18:04:33 -0700

    [dyad] wrote 2 file(s), deleted 42 file(s) + extra files edited outside of Dyad

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
@@ -30,7 +29,6 @@ const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
       <AuthProvider>
-        <Toaster />
         <Sonner />
         <BrowserRouter>
           <Routes>
