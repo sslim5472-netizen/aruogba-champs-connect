a7fb65d5373f4d06e378d48340beff9101292f8b
sslim5472-netizen <sslim5472@gmail.com>
2025-12-12 11:12:02 +0100

    Reverted all changes back to version 6a8bdd281fb3c569446dab7d93f9effb5601f2be

--- supabase/migrations/0070_updating_statistics_for_knights_fc_to_place_them_at_3rd_in_the_league_standings_.sql
+++
@@ -1,9 +0,0 @@
-UPDATE public.teams
-SET
-  wins = 3,
-  draws = 1,
-  losses = 1,
-  goals_for = 10,
-  goals_against = 5,
-  played = 5
-WHERE name = 'Knights FC';
--- supabase/migrations/0071_updating_statistics_for_enjoyment_fc_to_place_them_at_4th_in_the_league_standings_.sql
+++
@@ -1,9 +0,0 @@
-UPDATE public.teams
-SET
-  wins = 2,
-  draws = 3,
-  losses = 0,
-  goals_for = 8,
-  goals_against = 5,
-  played = 5
-WHERE name = 'Enjoyment FC';
